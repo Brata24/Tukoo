1ab75708198829ee81eb02eb616bfea49b261724
Daffa' Hakim <dcatproject@yahoo.com>
2025-10-28 08:12:21 +0700

    init prod domains

--- src/hooks.ts
+++ src/hooks.ts
@@ -3,8 +3,8 @@ import type { Reroute } from "@sveltejs/kit";
 
 export const reroute: Reroute = ({ url }) => {
 
-    if (url.hostname.endsWith('.beta.tukoo.test') || url.hostname.endsWith('.beta.tukoo.web.id')) {
-        const subdomain = url.hostname.replace('.beta.tukoo.test', '').replace('.beta.tukoo.web.id', '');
+    if (url.hostname.endsWith('.tukoo.test') || url.hostname.endsWith('.tukoo.web.id')) {
+        const subdomain = url.hostname.replace('.tukoo.test', '').replace('.tukoo.web.id', '');
         return `/pos/${subdomain}${url.pathname}`;
     }
     return url.pathname;
