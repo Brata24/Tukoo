38c894eb94df57fd3747e9d7a50746a4485f6cc0
Daffa' Hakim <dcatproject@yahoo.com>
2025-11-13 23:34:23 +0700

    fix routing storage

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,6 +2,11 @@ import type { Reroute } from "@sveltejs/kit";
 
 
 export const reroute: Reroute = ({ url }) => {
+    console.log('Rerouting for URL:', url.pathname);
+
+    if (url.pathname.startsWith('/storage/')) { 
+        return url.pathname;
+    }
 
     if (url.hostname.endsWith('.tukoo.test') || url.hostname.endsWith('.tukoo.web.id')) {
         const subdomain = url.hostname.replace('.tukoo.test', '').replace('.tukoo.web.id', '');
